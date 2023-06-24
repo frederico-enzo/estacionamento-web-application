b1be7186fa2390beb20a093972b124e2fbb9c12f
Frederico Enzo <frederico.enzo@hotmail.com>
2023-06-24 02:01:13 -0300

    Arrumei as Confi web Cors

--- API/src/main/java/br/com/uniamerica/estacionamento/config/WebConfig.java
+++ API/src/main/java/br/com/uniamerica/estacionamento/config/WebConfig.java
@@ -9,7 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
