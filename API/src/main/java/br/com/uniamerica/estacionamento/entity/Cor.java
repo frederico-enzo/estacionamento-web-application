b846ed14c3536386be01bff7aa2927d646de8b76
Frederico <frederico.enzo@hotmail.com>
2024-09-26 20:06:05 -0300

    recover

--- API/src/main/java/br/com/uniamerica/estacionamento/entity/Cor.java
+++
@@ -1,5 +0,0 @@
-package br.com.uniamerica.estacionamento.entity;
-
-public enum Cor {
-    BRANCO, AZUL, PRETO, AMARELO, VERMELHO, PRATA, ROSA, VERDE, ROXO
-}
--- API/src/main/java/br/com/uniamerica/estacionamento/entity/Tipo.java
+++
@@ -1,5 +0,0 @@
-package br.com.uniamerica.estacionamento.entity;
-
-public enum Tipo {
-    CARRO, MOTO, VAN
-}
