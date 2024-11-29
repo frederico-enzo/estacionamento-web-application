5562d8b0b2e24a2c6c85d53cec205af8cee2b8f8
Frederico <frederico.enzo@hotmail.com>
2024-11-29 17:40:22 -0300

    feat: integrar Font Awesome ao projeto e registrar ícones globais no main.js

--- interface/src/main.ts
+++ interface/src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).use(router).mount('#app')
+// Importações do Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUser, faExchangeAlt, faCar, faHome, faList, faTags, faCog } from '@fortawesome/free-solid-svg-icons';
+
+// Adicione os ícones à biblioteca
+library.add(faUser, faExchangeAlt, faCar, faHome, faList, faTags, faCog);
+
+// Criação do app Vue
+const app = createApp(App);
+
+// Use o router e registre o componente Font Awesome globalmente
+app.use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
