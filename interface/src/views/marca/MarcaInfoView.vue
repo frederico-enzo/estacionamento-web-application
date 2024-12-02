6bb3a6586bd7c0028119b04618398d1cdc1d230e
Frederico <frederico.enzo@hotmail.com>
2024-12-02 17:00:22 -0300

    feat: cria a view MarcaInfoView para exibir detalhes de uma marca

--- interface/src/views/marca/MarcaInfoView.vue
+++ interface/src/views/marca/MarcaInfoView.vue
@@ -1,5 +1,106 @@
 <template>
-    <div class="col-md-10 text-start">
-        <p class="fs-4 fw-semibold">Marcas</p>
+    <div class="container w-50">
+      <div class="row mt-5">
+        <div class="col-md-12 text-start">
+          <p class="fs-5">Detalhes da Marca</p>
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+  
+      <div class="row w-100 d-flex justify-content-center m-0">
+        <div class="mt-3 w-100 text-start">
+          <p class="fs-6"><span class="dado">Nome: </span> {{ marca.nome }} </p>
+        </div>
+      </div>
+      <div class="row w-100 d-flex justify-content-center m-0">
+        <div class="w-100 text-start">
+          <p class="fs-6"><span class="dado"> Status: </span>
+            <span v-if="marca.ativo" class="badge text-bg-success">
+              Ativo
+            </span>
+            <span v-if="!marca.ativo" class="badge text-bg-danger">
+              Desativado
+            </span>
+          </p>
+        </div>
+      </div>
+      <hr>
+      <div class="row mt-5">
+        <div class="col-md-12 text-start">
+          <p class="fs-5">Histórico</p>
+        </div>
+      </div>
+      <div class="row w-100 d-flex justify-content-center m-0">
+        <div class="w-100 text-start">
+          <p class="fs-6"><span class="dado">Data do Cadastro: </span>{{ formatDate(marca.cadastro) }}</p>
+        </div>
+      </div>
+      <div class="row w-100 d-flex justify-content-center m-0">
+        <div class="w-100 text-start">
+          <p class="fs-6"><span class="dado">Última edição: </span> {{ formatDate(marca.edicao) }}</p>
+        </div>
+      </div>
+  
+      <div class="row d-flex justify-content-center mt-3">
+        <div class="col-md-3">
+          <div class="d-grid gap-2">
+            <router-link type="button" class="btn btn-secondary" to="/marca">Voltar
+            </router-link>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <script lang="ts">
+  import { MarcaClient } from '@/service/Marca.client'
+  import { Marca } from '@/model/Marca'
+  import { defineComponent } from 'vue'
+  
+  export default defineComponent({
+    name: 'MarcaInfo',
+    data(): any {
+      return {
+        marca: new Marca(),
+      }
+    },
+    computed: {
+      id(): any {
+        return this.$route.query.id
+      }
+    },
+    mounted() {
+      if (this.id !== undefined) {
+        this.findById(Number(this.id))
+      }
+    },
+    methods: {
+      formatDate(dateString: string | number | Date) {
+        if (dateString != null) {
+          const dateTime = new Date(dateString)
+          const formattedDate = dateTime.toLocaleDateString()
+          const formattedTime = dateTime.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit'
+          })
+          return `${formattedDate} ${formattedTime}`
+        } else {
+          return '---'
+        }
+      },
+      findById(id: number) {
+        const marcaClient = new MarcaClient()
+        marcaClient
+          .findById(id)
+          .then(sucess => {
+            this.marca = sucess
+          })
+          .catch(error => {
+            this.mensagem.mensagem = error.response.data
+            this.mensagem.status = false
+            this.mensagem.ativo = true
+          })
+      }
+    }
+  })
+  </script>
