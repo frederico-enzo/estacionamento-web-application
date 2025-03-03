527d4fa469612281cdfed210e68074e1d9d568c7
Guilherme Franchin <gui.franchin12@gmail.com>
2025-03-03 19:23:18 -0300

    feat/ adding docker file && docker compose front and back

--- interface/src/service/Marca.client.ts
+++ interface/src/service/Marca.client.ts
@@ -6,7 +6,7 @@ export class MarcaClient {
 
   constructor() {
     this.axiosClient = axios.create({
-      baseURL: `http://localhost:8081/api`,
+      baseURL: `http://localhost:8080/api`,
       headers: { 'Content-type': 'application/json' }
     })
   }
